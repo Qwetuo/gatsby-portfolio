f04b93574c888c8cbac7abc93dbf601deadfd4f4
qwetuo <chewminzhuang@hotmail.com>
2018-11-26 14:47:56 +0800

    amended landing info

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,8 @@ const Header = (props) => (
       <img src={icon} alt="icon"/>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+              <h1>Min Zhuang</h1>
+              <p>An aspiring web developer passionate about using technology to improve lives.</p>
             </div>
         </div>
         <nav>
