e2fe46f721fa3e6e21d0313bd6d30fddc2860a11
qwetuo <chewminzhuang@hotmail.com>
2018-12-02 19:53:35 +0800

    Moved projects content into its own component Projects

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import About from "./About"
+import Projects from "./Projects"
 import Resume from "./Resume"
 import Contact from "./Contact"
-
-import pic02 from '../images/pic02.jpg'
 
 class Main extends React.Component {
   render() {
@@ -17,13 +16,7 @@ class Main extends React.Component {
 
         <About article={this.props.article} articleTimeout={this.props.articleTimeout} close={close}/>
 
-        <article id="projects" className={`${this.props.article === 'projects' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Projects</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-          {close}
-        </article>
+        <Projects article={this.props.article} articleTimeout={this.props.articleTimeout} close={close}/>
 
         <Resume article={this.props.article} articleTimeout={this.props.articleTimeout} close={close}/>
         
