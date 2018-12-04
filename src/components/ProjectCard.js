5a277e951406337a1ee4bbfe430cf9899231db7b
qwetuo <chewminzhuang@hotmail.com>
2018-12-04 23:51:26 +0800

    added function to toggle 'more info' in project's card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -11,6 +11,7 @@ const ProjectCard = props => (
         rel="noopener noreferrer"
         href={props.frontmatter.source_code}
         className="icon fa-github fa-lg"
+        onClick={e => e.stopPropagation()}
       >
         <span className="label">GitHub</span>
       </a>
@@ -19,6 +20,7 @@ const ProjectCard = props => (
         rel="noopener noreferrer"
         href={props.frontmatter.website}
         className="icon fa-external-link fa-lg"
+        onClick={e => e.stopPropagation()}
       >
         <span className="label">Link</span>
       </a>
@@ -30,6 +32,12 @@ const ProjectCard = props => (
         Built in {props.frontmatter.effort_days}+ days with{' '}
         {props.frontmatter.tags.join(' | ')}
       </p>
+      {props.thisIndexIsOpened ? (
+        <div
+          className="card-learn-more"
+          dangerouslySetInnerHTML={{ __html: props.html }}
+        />
+      ) : null}
     </div>
   </div>
 )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ProjectCard from './ProjectCard'
 
-const Projects = props => {
-  const projects = props.projects.sort(
-    (a, b) => b.node.frontmatter.priority - a.node.frontmatter.priority
-  )
-  return (
-    <article
-      id="projects"
-      className={`${props.article === 'projects' ? 'active' : ''} ${
-        props.articleTimeout ? 'timeout' : ''
-      }`}
-      style={{ display: 'none' }}
-    >
-      <h2 className="major">Projects</h2>
-      {projects.map(project => (
-        <ProjectCard frontmatter={project.node.frontmatter} />
-      ))}
-      {props.close}
-    </article>
-  )
+class Projects extends Component {
+  constructor() {
+    super()
+    this.state = {
+      indexOpened: undefined
+    }
+  }
+
+  toggleLearnMore(index) {
+    const indexOpenOrClose =
+      index === this.state.indexOpened ? undefined : index
+    this.setState({
+      indexOpened: indexOpenOrClose
+    })
+  }
+
+  render() {
+    const projects = this.props.projects.sort(
+      (a, b) => b.node.frontmatter.priority - a.node.frontmatter.priority
+    )
+    return (
+      <article
+        id="projects"
+        className={`${this.props.article === 'projects' ? 'active' : ''} ${
+          this.props.articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
+      >
+        <h2 className="major">Projects</h2>
+        {projects.map((project, index) => {
+          const thisIndexIsOpened =
+            this.state.indexOpened === index ? true : false
+          return (
+            <div onClick={() => this.toggleLearnMore(index)}>
+              <ProjectCard
+                key={index}
+                frontmatter={project.node.frontmatter}
+                html={project.node.html}
+                thisIndexIsOpened={thisIndexIsOpened}
+              />
+            </div>
+          )
+        })}
+        {this.props.close}
+      </article>
+    )
+  }
 }
 
 export default Projects
